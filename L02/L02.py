ba7f451ce4c38ddc8818add79bb804024f7402d6
tommyhj <tommyhj@kth.se>
2018-01-21 13:44:58 +0100

    Mer kommentarer

--- L02/L02.py
+++ L02/L02.py
@@ -3,6 +3,7 @@ from linkedQFile import LinkedQ
 
 
 def magic_trick(q, num_list):
+    ''' Uses the queue specified in q to perform a magic trick on the numbers listed in num_list'''
     switch = False
     for item in num_list:
         try:
@@ -24,14 +25,17 @@ def magic_trick(q, num_list):
 
 
 def amagic_trick():
+    ''' Takes input from the user for running the magick_trick function with the ArrayQ queue implementation'''
     print(magic_trick(ArrayQ(), input("Skriv in heltal, separerade av mellanslag ").split(" ")))
 
 
 def lmagic_trick():
+    ''' Takes input from the user for running the magick_trick function with the LinkedQ queue implementation'''
     print(magic_trick(LinkedQ(), input("Skriv in heltal, separerade av mellanslag ").split(" ")))
 
 
 def menu():
+    ''' A simple menu for the user to select which implementation of queue to use'''
     print("Trollkarlsprogram 1.0")
     print("Välj spelmotor:")
     print("1. Länkad lista")
@@ -51,5 +55,4 @@ def menu():
             val = input("Felaktig inmatning, försök igen: ")
 
 
-while True:
-    menu()
+menu()
