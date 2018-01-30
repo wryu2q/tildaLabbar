06b6ba3e9c375b0aa0aa4f6c07c2c365ed03cf09
Vidar Greinsmark <vidarg@kth.se>
2018-01-30 19:07:33 +0100

    försök på att fatta grafer

---
+++ L04/L04.py
@@ -0,0 +1,14 @@
+from bfs import *
+Class Graph:
+    ''' Handles graph trees '''
+    def __init__(self):
+        self.__adjacency_list = []
+        
+
+    def addVertex(self, node):
+        self.__adjacency_list.append(node)
+        
+
+
+        
+        
